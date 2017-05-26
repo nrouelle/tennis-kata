75b746353a1d87d321cff5394ec93d7f7308107a
Nadege Rouelle <Nadege Rouelle>
2017-05-26 21:44:52 +0200

    First commit

---
+++ KataTennis.Test/JeuTest.cs
@@ -0,0 +1,69 @@
+﻿using NFluent;
+using NUnit.Framework;
+
+namespace KataTennis.Test
+{
+    [TestFixture]    
+    public class JeuTest
+    {
+        [Test]
+        public void UnJoueurCommenceAvec0Points()
+        {
+            var game = new Game("Nadege", "Batiste");
+            var result = game.GetScore();
+
+            Check.That(result).Equals("Love all");
+        }
+
+        [Test]
+        public void WhenPlayerAHasAtLeast4pointsAnd2PointsMoreTheOpponentThenGameIsWon()
+        {
+            var game = new Game("Nadege", "Batiste");
+            game.PlayerAScore();
+            game.PlayerAScore();
+            game.PlayerAScore();
+            game.PlayerBScore();
+            game.PlayerAScore();
+            Check.That(game.GetScore()).Equals("Nadege wins");
+        }
+
+        [Test]
+        public void WhenPlayerBHasAtLeast4pointsAnd2PointsMoreTheOpponentThenGameIsWon()
+        {
+            var game = new Game("Nadege", "Batiste");
+            game.PlayerBScore();
+            game.PlayerBScore();
+            game.PlayerBScore();
+            game.PlayerAScore();
+            game.PlayerBScore();
+            Check.That(game.GetScore()).Equals("Batiste wins");
+        }
+
+        [Test]
+        public void WhenPlayersHave3PointsAndScoreAreEqualsThenGameIsDeuce()
+        {
+            var game = new Game("Nadege", "Batiste");
+            game.PlayerBScore();
+            game.PlayerBScore();
+            game.PlayerBScore();
+            game.PlayerAScore();
+            game.PlayerAScore();
+            game.PlayerAScore();
+            Check.That(game.GetScore()).Equals("forty all");
+        }
+
+        [Test]
+        public void WhenPlayersHave3PointsAndAPlayerHasOneMorePointThenGameIsAdvantage()
+        {
+            var game = new Game("Nadege", "Batiste");
+            game.PlayerBScore();
+            game.PlayerBScore();
+            game.PlayerBScore();
+            game.PlayerBScore();
+            game.PlayerAScore();
+            game.PlayerAScore();
+            game.PlayerAScore();
+            Check.That(game.GetScore()).Equals("Advantage Batiste");
+        }
+    }
+}
---
+++ KataTennis/Game.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace KataTennis
+{
+    public class Game
+    {
+        private readonly string _joueurAName;
+        private readonly string _joueurBName;
+
+        private int joueurAScore;
+        private int joueurBScore;
+
+        public Game(string joueurAName, string joueurBName)
+        {
+            _joueurAName = joueurAName;
+            _joueurBName = joueurBName;
+        }
+
+        public string GetScore()
+        {
+            if (HasWinner())
+            {
+                return GetPlayerWithHigherScore() + " wins";
+            }
+            if (IsDeuce())
+            {
+                return Translate(joueurAScore) + " all";
+            }
+            if (IsAdvantage())
+            {
+                return "Advantage " + GetPlayerWithHigherScore();
+            }
+            return "test";
+        }
+
+        private bool IsAdvantage()
+        {
+            if (joueurAScore >= 3 && joueurBScore >= 3 
+                && (joueurAScore == joueurBScore + 1 || joueurBScore == joueurAScore + 1))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private bool IsDeuce()
+        {
+            if (joueurAScore >= 3 && joueurBScore >= 3 && joueurBScore == joueurAScore)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private bool HasWinner()
+        {
+            if (joueurAScore >= 4 && joueurAScore > joueurBScore + 2 || joueurBScore >= 4 && joueurBScore > joueurAScore + 2)
+                return true;
+            return false;
+        }
+
+        public string GetPlayerWithHigherScore()
+        {
+            if (joueurAScore > joueurBScore)
+                return _joueurAName;
+            if (joueurAScore < joueurBScore)
+                return _joueurBName;
+            return "";
+        }
+
+        private string Translate(int score)
+        {
+            switch (score)
+            {
+                case 0:
+                    return "Love";
+                case 1:
+                    return "fifteen";
+                case 2:
+                    return "thirty";
+                case 3:
+                    return "forty";
+            }
+            throw new ArgumentOutOfRangeException();
+        }
+
+        public void PlayerAScore()
+        {
+            joueurAScore++;
+        }
+
+        public void PlayerBScore()
+        {
+            joueurBScore++;
+        }
+    }
+}
